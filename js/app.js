f9c39b90bc747c93d550e308ccfe317644be7e84
cowang01 <g_cowan27@hotmail.com>
2019-06-17 08:37:06 +0100

    ccwk6d5-DOMv2

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
+
+  const form = document.querySelector('.form');
+  form.addEventListener('click', handleFormClick);
+
 })
+
+const handleFormClick = function(event){
+  const updatedForm = event.preventDefault();
+  const resultForm = document.querySelector('#form_info-result');
+  resultForm.textContent = `Who is the burger for: ${this.name.value}`;
+}
